27a1dfa029f0a044a9285d5893ce92543643ea7a
Jolled <markus.persson@gmail.com>
2015-05-24 15:39:21 +0200

    small updates

--- run_analysis.R
+++ run_analysis.R
@@ -1,7 +1,18 @@
+# Coursera Getting and Cleaning Data - Course Project
+
+# This script will perform the following steps on the UCI HAR Dataset downloaded from 
+# https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip 
+# 1. Merge the training and the test sets to create one data set.
+# 2. Extract only the measurements on the mean and standard deviation for each measurement. 
+# 3. Use descriptive activity names to name the activities in the data set
+# 4. Appropriately label the data set with descriptive activity names. 
+# 5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
+
+
 library(plyr)
 setwd("C:/MyData/R/GettingAndCleaningData")
 
-#Download & Unzip File
+#Download & Unzip File with raw data
 fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 download.file(fileUrl,destfile="rawData.zip")
 unzip(zipfile="rawData.zip")
@@ -39,7 +50,7 @@ columnNames <- names(allData)
 wantedColumnsId <- grep("mean\\(\\)|std\\(\\)", columnNames)
 wantedColumnsNames <- c("subjectId","activityId",columnNames[wantedColumnsId])
 
-#Create subseted wanted tidy dataset
+#Create subseted wanted tidy dataset with only mean and std columns
 wantedData <- allData[,wantedColumnsNames]
 
 #Add column - activityNames
